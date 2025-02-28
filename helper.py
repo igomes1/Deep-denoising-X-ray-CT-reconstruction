de2eadfe3f999007e21dde4ff087d3398f15cb15
Ismael <ismael.gomesalmadaguillemin@epfl.ch>
2025-02-28 19:32:14 +0100

    add convolution with periodic boundary condition + rslt of optimization using GD

--- helper.py
+++ helper.py
@@ -11,4 +11,11 @@ def get_2D_Gaussian_kernel(sigma_h):
     # 2D kernel
     kernel_2D = kernel_1D.reshape(-1, 1) @ kernel_1D.reshape(1, -1)
     return kernel_2D
-    
+
+# transform kernel (of shape (2k+1) x (2k+1)) to have same shape of the img to which it's applied with periodic boundary condition
+def kernel_to_image(kernel, img):
+    img_kernel = np.zeros(img.shape)
+    k = kernel.shape[0] // 2
+    img_kernel[:(2*k+1), :(2*k+1)] = kernel
+    img_kernel = np.roll(img_kernel, (-k, -k), axis=(0, 1))
+    return img_kernel
